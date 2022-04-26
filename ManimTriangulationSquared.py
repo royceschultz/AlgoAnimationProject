56a79c74188fb954daa5904aef0b928a851c7ecf
Royce Schultz <rosc1351@colorado.edu>
2022-04-26 18:19:14 -0400

    refinements: Better colors, text, and functions

--- ManimTriangulationSquared.py
+++ ManimTriangulationSquared.py
@@ -2,43 +2,57 @@ from manim import *
 import numpy as np
 import json
 
-config.max_files_cached = 256
+np.random.seed(42)
 
+# Configuration
 POLYGON = json.load(open("polygons.json"))
 KEY = 'sharp'
 POINTS = POLYGON[KEY]
 PRIMARY_COLOR = PURPLE_B
 TIME_SHORT = 0.5
+MAX_DEPTH = 4
 
-message_text = Text('.').shift(3 * DOWN)
-def UpdateMessage(self, message):
-    global message_text
-    self.play(Unwrite(message_text), run_time=TIME_SHORT)
-    message_text = Text(message).shift(3 * DOWN)
-    self.play(Write(message_text), run_time=TIME_SHORT)
+# Manim Config
+config.max_files_cached = 256
+config.output_file = f'Triangulation_{KEY}.mp4'
+
+messages = []
+def ClearMessage(self):
+    global messages
+    if messages:
+        self.play(*[FadeOut(x) for x in messages], run_time=TIME_SHORT)
+        messages = []
+
+def UpdateMessage(self, lines):
+    global messages
+    if type(lines) == str:
+        lines = [lines]
+    ClearMessage(self)
+    messages.append(Text(lines[0]).shift(2.5 * DOWN))
+    for i in range(1, len(lines)):
+        messages.append(Text(lines[i]).next_to(messages[-1], DOWN))
+    self.play(*[Write(x) for x in messages], run_time=TIME_SHORT)
+    # for message in messages:
+    #     self.play(Write(message), run_time=TIME_SHORT)
     self.wait()
 class Triangulation(Scene):
     def construct(self):
         primary_polygon = Polygon(*[x + [0]  for x in POINTS], color=PRIMARY_COLOR)
         self.add(primary_polygon)
-        self.wait()
-        self.add(message_text)
         Triangulate(self, POINTS)
-        UpdateMessage(self, '.')
+        ClearMessage(self)
 
 def Triangulate(self, poly_points, depth=0):
     # Recursively find a diagonal to split the polygon into 2 sub-polygons
     # Base Case: If the polygon has 3 points, it is a triangle
     self.next_section()
-    if depth >= 4:
+    if depth >= MAX_DEPTH:
         return
     if len(poly_points) <= 3:
         return
-    selection = Polygon(*[x + [0] for x in poly_points], color=GREEN)
+    selection = Polygon(*[x + [0] for x in poly_points], color=WHITE)
     selection.set_fill(PRIMARY_COLOR, opacity=0.3)
     self.play(FadeIn(selection))
-    self.wait()
-    
     UpdateMessage(self, f'Finding a diagonal at depth {depth}')
     # Find the diagonal to split the polygon
     i, j = FindDiagonal(self, poly_points)
@@ -47,6 +61,11 @@ def Triangulate(self, poly_points, depth=0):
     # Partition on diagonal
     left = poly_points[i:j+1]
     right = poly_points[j:] + poly_points[:i+1]
+    # Randomize start point
+    x = np.random.randint(0, len(left))
+    left = left[x:] + left[:x]
+    x = np.random.randint(0, len(right))
+    right = right[x:] + right[:x]
     # Recursively triangulate both sides
     self.play(selection.animate.set_color(PRIMARY_COLOR))
     Triangulate(self, left, depth+1)
@@ -87,13 +106,19 @@ def FindDiagonal(self, poly_points):
     # Scan all edges for intersections with the bisecting ray.
     # Find closest intersection.
     k = None
-    intersection_dot = Dot(b + [0])
+    intersection_dot = Dot(closest_intersection + [0])
+    old_selected_edge = None
     for i in range(2, len(poly_points)):
         # Check if the edge intersects the bisecting ray
         edge = (poly_points[i], poly_points[i-1])
         # Animate edge selection
-        selected_edge_line = Line(edge[0] + [0], edge[1] + [0], color=YELLOW)
-        self.play(FadeIn(selected_edge_line), run_time=TIME_SHORT)
+        # make selected line bold
+        selected_edge_line = Line(edge[0] + [0], edge[1] + [0], color=YELLOW, stroke_width=10)
+        if old_selected_edge is not None:
+            self.play(FadeIn(selected_edge_line), FadeOut(old_selected_edge), run_time=TIME_SHORT)
+        else:
+            self.play(FadeIn(selected_edge_line), run_time=TIME_SHORT)
+        old_selected_edge = selected_edge_line
 
         intersection = FindIntersection(edge, (b, closest_intersection))
         if intersection is not None:
@@ -108,22 +133,17 @@ def FindDiagonal(self, poly_points):
             closest_intersection = intersection
             k = i
         
-        # Cleanup edge selection loop
-        self.play(FadeOut(selected_edge_line), run_time=TIME_SHORT)
+    # Cleanup edge selection loop
+    self.play(FadeOut(old_selected_edge), run_time=TIME_SHORT)
 
     # Use closest intersection edge to partition the rest of the points
     # Left = poly_points[:k]
     # Right = poly_points[k:]
 
-    # Animate partitioning
-    dot_k = Dot(poly_points[k] + [0], color=BLUE)
-    dot_km1 = Dot(poly_points[k-1] + [0], color=BLUE)
-    self.play(FadeIn(dot_k), FadeIn(dot_km1))
-
     #
     # Search LEFT partition
     #
-    UpdateMessage(self, 'Search left partition')
+    UpdateMessage(self, ['Initialize a triangle on the', 'left side of the intersection edge'])
     selected_index = k - 1
     tri_points = [b, closest_intersection, poly_points[selected_index]]
     selected_triangle = Polygon(*[x + [0] for x in tri_points], color=YELLOW)
@@ -131,6 +151,7 @@ def FindDiagonal(self, poly_points):
     self.play(FadeIn(selected_triangle))
     query_point_dot = Dot(poly_points[selected_index] + [0], color=RED)
     self.play(Create(query_point_dot))
+    UpdateMessage(self, 'Search left partition for a point in the triangle')
     for i, query_point in reversed(list(enumerate(poly_points[1:k-1]))):
         # Animate query_point_selection
         new_query_point_dot = Dot(query_point + [0], color=RED)
@@ -145,10 +166,11 @@ def FindDiagonal(self, poly_points):
 
     if selected_index != 1:
         # Found a diagonal!
+        self.play(selected_triangle.animate.set_fill(GREEN, opacity=0.3).set_stroke(GREEN), run_time=TIME_SHORT)
         UpdateMessage(self, 'Found a diagonal!')
         partition_line = Line(b + [0], poly_points[selected_index] + [0], color=BLUE)
         # Clean up plot
-        self.play(FadeOut(dot_k), FadeOut(dot_km1), FadeOut(query_point_dot), FadeOut(intersection_dot), FadeOut(bray_line))
+        self.play(FadeOut(query_point_dot), FadeOut(intersection_dot), FadeOut(bray_line))
         # Show new partition line
         self.play(Create(partition_line))
         self.play(FadeOut(selected_triangle))
@@ -157,8 +179,9 @@ def FindDiagonal(self, poly_points):
 
     # No diagonal from b to left polygon
     # Turn min line red (invalid)
-    self.play(selected_triangle.animate.set_fill(RED, opacity=0.3), selected_triangle.animate.set_stroke(RED))
-    UpdateMessage(self, 'No Good!')
+    # Set fill and color
+    self.play(selected_triangle.animate.set_fill(RED, opacity=0.3).set_stroke(RED))
+    UpdateMessage(self, ['No Good!', 'Triangle contains a polygon edge.'])
     self.play(FadeOut(selected_triangle))
     # Save to remove later
     old_query_point_dot = query_point_dot
@@ -166,7 +189,8 @@ def FindDiagonal(self, poly_points):
     #
     # Search RIGHT partition
     #
-    UpdateMessage(self, 'Search right partition')
+    UpdateMessage(self, 'Search the other side for a diagonal')
+    UpdateMessage(self, ['Initialize a triangle on the' , 'right side of the intersection edge'])
     selected_index = k
     tri_points = [b, closest_intersection, poly_points[selected_index]]
     selected_triangle = Polygon(*[x + [0] for x in tri_points], color=YELLOW)
@@ -174,6 +198,7 @@ def FindDiagonal(self, poly_points):
     self.play(FadeIn(selected_triangle))
     query_point_dot = Dot(poly_points[selected_index] + [0], color=RED)
     self.play(Create(query_point_dot))
+    UpdateMessage(self, 'Search right partition for a point in the triangle')
     for i, query_point in enumerate(poly_points[k+1:]):
         # Animate query point selection
         new_query_point_dot = Dot(query_point + [0], color=RED)
@@ -188,24 +213,23 @@ def FindDiagonal(self, poly_points):
 
     if selected_index != len(poly_points) - 1:
         # Found a valid diagonal (b -> point with minimum angle)
+        self.play(selected_triangle.animate.set_fill(GREEN, opacity=0.3).set_stroke(GREEN), run_time=TIME_SHORT)
         UpdateMessage(self, 'Found a diagonal!')
-
         # Clean up plot
-        self.play(FadeOut(dot_k), FadeOut(dot_km1), FadeOut(bray_line), FadeOut(intersection_dot), FadeOut(query_point_dot), FadeOut(old_query_point_dot))
+        self.play(FadeOut(bray_line), FadeOut(intersection_dot), FadeOut(query_point_dot), FadeOut(old_query_point_dot))
         # Add triangulation edge
         partition_line = Line(b + [0], poly_points[selected_index] + [0], color=BLUE)
         self.play(Create(partition_line))
         self.play(FadeOut(selected_triangle))
-
         return 0, selected_index
     # Else unsuccessful search on the right side
-    self.play(selected_triangle.animate.set_fill(RED, opacity=0.3), selected_triangle.animate.set_stroke(RED))
-    UpdateMessage(self, 'No Good!\nDiagonal exists between left and right partitions')
+    self.play(selected_triangle.animate.set_fill(RED, opacity=0.3).set_stroke(RED))
+    UpdateMessage(self, ['No Good!', 'Triangle contains a polygon edge.'])
     self.play(FadeOut(selected_triangle))
+    UpdateMessage(self, ['The diagonal exists between', 'the left and right partitions'])
     # Found a proper ear between left and right sides
-    self.play(FadeOut(selected_triangle))
     partition_line = Line(a + [0], c + [0], color=BLUE)
-    self.play(FadeOut(dot_k), FadeOut(dot_km1), FadeOut(bray_line), FadeOut(intersection_dot))
+    self.play(FadeOut(bray_line), FadeOut(intersection_dot))
     self.play(Create(partition_line))
     self.play(FadeOut(query_point_dot), FadeOut(old_query_point_dot))
     return 1, len(poly_points) - 1
