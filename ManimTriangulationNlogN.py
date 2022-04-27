91504c39da9bf94a33468e15bf01014e5e110d24
Royce Schultz <rosc1351@colorado.edu>
2022-04-27 15:28:34 -0400

    Adding some useful helper functions. Debugging with matplotlib

--- ManimTriangulationNlogN.py
+++ ManimTriangulationNlogN.py
@@ -1,3 +1,16 @@
+from manim import *
+import numpy as np
+import json
+
+from matplotlib import pyplot as plt
+
+
+# Configuration
+POLYGON = json.load(open("polygons.json"))
+KEY = 'large'
+POINTS = POLYGON[KEY]
+
+
 class Node:
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -16,43 +29,41 @@ def Triangulate(points):
     # return: list of triangles
     # TODO: Implement this method
 
+    # DEBUG PLOT
+    for i in range(len(points)):
+        edge = np.array([points[i-1], points[i]])
+        plt.plot(edge[:, 0], edge[:, 1], 'k-')
+
     # Sort points in order of x (or y)
-    trees = BST()
+    # TODO: We can probably use arg sort instead.
+    sorted_points = np.argsort([x[0] for x in points])
+    segments = BST()
     sub_polygons = []
-    for point in points:
-        pass
+    for i in sorted_points:
+        point = points[i]
         # what type of point is it?
         # Assume clockwise orientation
         # Scanline travels top to bottom
-        # start vertex?
-            # both edges point down
-            # interior angle is < 180
-        # end vertex?
-            # both edges point up
-            # interior angle is < 180
-        # split vertex?
-            # both edges point down
-            # interior angle is > 180
-        # merge vertex?
-            # both edges point up
-            # interior angle is > 180
-        # interior vertex?
-            # one edge points up, one points down
-            # Does left or right matter?
 
         # if start vertex:
+        tri_points = [points[i-1], point, points[(i+1) % len(points)]]
+        c = 'm'
+        if IsStartVertex(tri_points):
             # add edges to tree
-        # if end vertex:
+            c = 'y' # DEBUG PLOT
+        elif IsEndVertex(tri_points):
             # look at the vertex's edges
             # if helper(edge) is a merge vertex:
                 # add a diagonal from v to helper(edge)
             # remove edge from tree
-        # if split vertex:
+            c = 'r' # DEBUG PLOT
+        elif IsSplitVertex(tri_points):
             # find the edge to it's left
             # add a diagonal from v to helper(edge)
             # set hepder(edge) to v
             # add v's edges to the tree
-        # if merge vertex:
+            c = 'b'
+        elif IsMergeVertex(tri_points):
             # look at the edge terminating at v
             # if helper(edge) is a merge vertex:
                 # add a diagonal from v to helper(edge)
@@ -61,9 +72,9 @@ def Triangulate(points):
             # if helper(edge) is a merge vertex:
                 # add a diagonal from v to helper(edge)
             # helper(edge) = v
-        
-
-        # Potentially add edges and split polygons
+            c = 'g'
+        plt.plot(point[0], point[1], 'o', color=c)
+    plt.show()
     
     for polygon in sub_polygons:
         pass
@@ -91,3 +102,77 @@ def MonotoneTriangulate(points):
             # do some triangulating
 
         # how to terminate?
+
+#
+# Helpers
+#
+def InteriorAngle(a, b):
+    # v1, v2: vectors
+    # return: angle in degrees
+    # Define Interior:
+    # Assume clockwise orientation
+    # v1 points backwards, v2 points forwards
+    # left of v1 and right of v2 is interior.
+    angle = np.arctan2(a[1], a[0]) - np.arctan2(b[1], b[0])
+    if angle > 2 * np.pi:
+        angle -= 2 * np.pi
+    if angle < 0:
+        angle += 2 * np.pi
+    return angle
+
+def IsStartVertex(tri_points):
+    # Parameters:
+    # tri_points: list of 3 points, query vertex is tri_points[1]
+    # Assumes clockwise orientation
+    a, b, c = tri_points
+    v1 = np.subtract(a, b)
+    v2 = np.subtract(c, b)
+    # both edges point down
+    if v1[1] < 0 and v2[1] <= 0:
+        angle = InteriorAngle(v2, v1)
+        # assert angle > 0
+        if angle < np.pi:
+            return True
+    return False
+
+def IsEndVertex(tri_points):
+    a, b, c = tri_points
+    v1 = np.subtract(a, b)
+    v2 = np.subtract(c, b)
+    # both edges point up
+    if v1[1] > 0 and v2[1] > 0:
+        angle = InteriorAngle(v2, v1)
+        # assert angle > 0
+        if angle < np.pi:
+            return True
+    return False
+
+def IsSplitVertex(tri_points):
+    a, b, c = tri_points
+    v1 = np.subtract(a, b)
+    v2 = np.subtract(c, b)
+    if v1[1] < 0 and v2[1] <= 0:
+        angle = InteriorAngle(v2, v1)
+        # assert angle > 0
+        if angle > np.pi:
+            return True
+    return False
+
+def IsMergeVertex(tri_points):
+    a, b, c = tri_points
+    v1 = np.subtract(a, b)
+    v2 = np.subtract(c, b)
+    if v1[1] > 0 and v2[1] > 0:
+        angle = InteriorAngle(v2, v1)
+        # assert angle > 0
+        if angle > np.pi:
+            return True
+    return False
+
+# interior vertex?
+    # one edge points up, one points down
+    # Does left or right matter?
+
+
+if __name__ == "__main__":
+    Triangulate(POINTS)
