13a2a372e9975f13f99165d11487e77d36088990
Royce Schultz <rosc1351@colorado.edu>
2022-04-27 14:25:55 -0400

    More algorithm sketching

--- ManimTriangulationNlogN.py
+++ ManimTriangulationNlogN.py
@@ -22,13 +22,46 @@ def Triangulate(points):
     for point in points:
         pass
         # what type of point is it?
-        # start vertex? both edges point up
-        # end vertex? both edges point down
-        # side vertex? one edge points up, one points down (left or right?)
-        # if start or end vertex, add to tree
-        # if a side vertex, attach at node in the tree
-            # is this the closest node?
-            # does left/right matter?
+        # Assume clockwise orientation
+        # Scanline travels top to bottom
+        # start vertex?
+            # both edges point down
+            # interior angle is < 180
+        # end vertex?
+            # both edges point up
+            # interior angle is < 180
+        # split vertex?
+            # both edges point down
+            # interior angle is > 180
+        # merge vertex?
+            # both edges point up
+            # interior angle is > 180
+        # interior vertex?
+            # one edge points up, one points down
+            # Does left or right matter?
+
+        # if start vertex:
+            # add edges to tree
+        # if end vertex:
+            # look at the vertex's edges
+            # if helper(edge) is a merge vertex:
+                # add a diagonal from v to helper(edge)
+            # remove edge from tree
+        # if split vertex:
+            # find the edge to it's left
+            # add a diagonal from v to helper(edge)
+            # set hepder(edge) to v
+            # add v's edges to the tree
+        # if merge vertex:
+            # look at the edge terminating at v
+            # if helper(edge) is a merge vertex:
+                # add a diagonal from v to helper(edge)
+            # remove edge from tree
+            # look at the edge to the left of v
+            # if helper(edge) is a merge vertex:
+                # add a diagonal from v to helper(edge)
+            # helper(edge) = v
+        
 
         # Potentially add edges and split polygons
     
@@ -38,5 +71,23 @@ def Triangulate(points):
 
 def MonotoneTriangulate(points):
     # Check points are monotone
-    # Divide into 2 sets
+    # Divide into 2 sets, sorted in monotone direction (tbd)
+    left_points, right_points = MonotoneSplit(points)
     # Greedy triangulate
+
+    # 2 stacks
+    left_stack = []
+    right_stack = []
+
+    while True:
+        pass
+        # if left_points.top() < right_points.top():
+            # left_stack.push(left_points.pop())
+        # else:
+            # right_stack.push(right_points.pop())
+        # expect each stack to have 1 element
+        # 1 stack may exceed this IF nodes are reflex (angle > 180, sweeping outward)
+        # if both stacks have more than 1 element
+            # do some triangulating
+
+        # how to terminate?
